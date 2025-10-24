0af3d20d5a8f55614b367f5a08da79f0688cf69a
Umberto Novellis <umberto.novellis@gmail.com>
2025-10-24 10:17:27 +0200

    async select

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,31 +1,57 @@
 import { AiOutlineArrowRight } from "react-icons/ai";
+import AsyncSelect from "react-select/async";
 import useData from "../hooks/useData";
+
 interface Props {
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   cityName: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onFetch: () => void;
 }
 
 function InputField(props: Props) {
-  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+  const { autoData, autoLoading, autoError, autocomplete } = useData();
+
+  // funzione che react-select usa per caricare le opzioni
+  const loadOptions = async (inputValue: string, callback: any) => {
+    if (!inputValue) {
+      callback([]);
+      return;
+    }
+    await autocomplete(inputValue); // chiama la tua funzione che fetcha i dati
+    if (autoData) {
+      // mappa i dati ricevuti in opzioni compatibili con react-select
+      const options = autoData.map((city: any) => ({
+        label: city.name,
+        value: city.id,
+      }));
+      callback(options);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
     if (e.key === "Enter") {
       props.onFetch();
     }
-  }
-  const { autoData, autoLoading, autoError, autocomplete } = useData();
+  };
+
   return (
-    <div className="flex justify-center mt-10 bg-blue-100  w-52 mx-auto rounded-2xl  items-center relative ">
-      <input
-        type="text"
-        id="cityName"
-        className="bg-blue-100 rounded-md"
-        placeholder="Inserisci nome città"
-        onChange={props.onChange}
-        value={props.cityName}
+    <div className="flex flex-col justify-center mt-10 w-52 mx-auto relative">
+      <AsyncSelect
+        cacheOptions
+        loadOptions={loadOptions}
+        defaultOptions
+        onChange={(selectedOption: any) => {
+          // aggiornare l'input con la città selezionata
+          props.onChange({
+            target: { value: selectedOption?.label },
+          } as React.ChangeEvent<HTMLInputElement>);
+        }}
         onKeyDown={handleKeyDown}
+        placeholder="Inserisci nome città"
       />
+
       <button
-        className="bg-green-700 rounded-sm py-1 px-1.5 absolute right-0 z-10"
+        className="bg-green-700 rounded-sm py-1 px-1.5 absolute right-0 top-1/2 transform -translate-y-1/2 z-10"
         onClick={props.onFetch}
       >
         <AiOutlineArrowRight className="text-amber-300 " />
